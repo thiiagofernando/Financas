c1ee5b188267567bb0de8497a820fc9bb44352c3
thiag <thiag@DARTHVADER>
2017-04-17 00:24:30 -0400

    Projeto Finalizado

--- Financas/Controllers/HomeController.cs
+++ Financas/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace Financas.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         // GET: Home
---
+++ Financas/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace Financas.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autentica(String login, String senha)
+        {
+            if(WebSecurity.Login(login, senha))
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ModelState.AddModelError("login.Invalido", "Login ou senha Incorretos");
+                return View("Index");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            WebSecurity.Logout();
+            return RedirectToAction("Index");
+        }
+    }
+}
